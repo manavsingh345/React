fcfc0688101e3b9bc05d12305331d3b37308136c
Manav Singh <manavchaudhary060705@gmail.com>
2025-05-30 10:54:13 +0530

    Keep only Lecture9.4 on GitHub

--- Lecture10.1/src/Class11Program.jsx
+++
@@ -1,7 +0,0 @@
-export default function Class11Program() {
-    return (
-        <div>
-            <h1>  NEET programs for Class 11th.</h1>
-        </div>
-    )
-}
--- Lecture10.1/src/ErrorPage.jsx
+++
@@ -1,7 +0,0 @@
-export default function LandingPage() {
-    return (
-        <div>
-            <h1>Something went Wrong! </h1>
-        </div>
-    )
-}

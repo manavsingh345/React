1ff70385f6ef8625856a0f3adfa438ffd07f1670
Manav Singh <manavchaudhary060705@gmail.com>
2025-05-31 10:16:13 +0530

    Custom Hooks

---
+++ CustomHooks/src/App.jsx
@@ -0,0 +1,25 @@
+import { useState } from 'react'
+import './App.css'
+
+function useCounter() {
+  const [count, setCount] = useState(0)
+  function increaseCount() {
+    setCount(count + 1);
+  }
+  return {
+    count: count,
+    increaseCount: increaseCount
+  }
+}
+
+function App() {
+
+  const { count, increaseCount } = useCounter();
+  return (
+    <div>
+      <button onClick={increaseCount}>Increase: {count}</button>
+    </div>
+  )
+}
+
+export default App

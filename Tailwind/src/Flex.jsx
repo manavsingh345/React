c26b9961902b75344e69745b8d62890878043dd0
Manav Singh <manavchaudhary060705@gmail.com>
2025-06-02 08:09:43 +0530

    Tailwind

---
+++ Tailwind/src/Flex.jsx
@@ -0,0 +1,10 @@
+function Flex(){
+    return(
+        <div style={{display:"flex", justifyContent:"space-between"}}>
+            <div className='bg-red-500'>Hi there, I am learning flex</div>
+            <div style={{backgroundColor:"green"}}>Hi there, I am learning flex</div>
+            <div style={{backgroundColor:"blue"}}>Hi there, I am learning flex</div>
+        </div>
+    )
+}
+export default Flex;

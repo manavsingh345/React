41ac0082e38b9b153cc366e7c43e8363d2ae3166
Manav Singh <manavchaudhary060705@gmail.com>
2025-06-02 10:01:56 +0530

    Learning tailwind

--- Tailwind/src/Flex.jsx
+++ Tailwind/src/Flex.jsx
@@ -1,9 +1,19 @@
-function Flex(){
-    return(
-        <div style={{display:"flex", justifyContent:"space-between"}}>
-            <div className='bg-red-500'>Hi there, I am learning flex</div>
-            <div style={{backgroundColor:"green"}}>Hi there, I am learning flex</div>
-            <div style={{backgroundColor:"blue"}}>Hi there, I am learning flex</div>
+function Flex() {
+    return (
+        <div>
+            <div className='grid  sm:grid-cols-12'>
+                <div className='col-span-12 sm:col-span-5 bg-red-500'>Hi there, I am learning flex</div>
+                <div className='col-span-12 sm:col-span-5 bg-blue-500'>Hi there, I am learning flex</div>
+                <div className='col-span-12 sm:col-span-2 bg-green-800'>Hi there, I am learning flex</div>
+                
+            </div>
+
+            <div className='flex justify-around'>
+
+                <div className='sm:bg-blue-400 bg-red-500 text-2xl'>Hi there, I am learning flex</div>
+                <div className='bg-blue-500 rounded-md'>Hi there, I am learning flex</div>
+                <div className='bg-green-800'>Hi there, I am learning flex</div>
+            </div>
         </div>
     )
 }
